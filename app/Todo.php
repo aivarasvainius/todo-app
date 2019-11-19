c5fda695995b5e7605dbde3b92773aab1f1e0c20
Aivaras Vainius <aivaras.vainius@gmail.com>
2019-11-19 18:32:38 +0200

    Todo model

---
+++ app/Todo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    /**
+     * The fields that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['text'];
+}
